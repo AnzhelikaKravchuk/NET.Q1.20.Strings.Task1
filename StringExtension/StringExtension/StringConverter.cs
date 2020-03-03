8ea5f773b75ea575f03a5bcb42d9072c3c949d3f
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2020-03-03 16:36:19 +0300

    Initial commit

---
+++ StringExtension/StringExtension/StringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringExtension
+{
+    /// <summary>
+    /// StringConverter.
+    /// </summary>
+    public static class StringConverter
+    {
+        /// <summary>
+        /// Converts the specified source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="count">The count.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">source or count</exception>
+        public static string Convert(string source, int count)
+        {
+           throw  new NotImplementedException();
+        }
+    }
+}
